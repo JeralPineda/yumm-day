312a96917710b8a9b45b5902f08d065d8f076031
Jeral Pineda <jeral.cris@gmail.com>
2024-06-30 22:28:19 -0600

    fix: add config oauth

--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -1,9 +1,13 @@
+import React, { useState } from "react";
 import { Linking, View } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { Button } from "@/components/ui/button";
 import Logo from "@/components/logo";
 import { account } from "@/lib/appwrite/config";
 import { OAuthProvider } from "react-native-appwrite";
+
+import * as Google from "expo-auth-session/providers/google";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 //* client id web: 54442420294-vuueq211oj24kf8t78t1r0abvlkqcev7.apps.googleusercontent.com - secret : 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
 
@@ -12,6 +16,15 @@ import { OAuthProvider } from "react-native-appwrite";
 
 export default function SignIn() {
   // const { signIn } = useSession();
+
+  const [userInfo, setUserInfo] = useState(null);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId: "4442420294-k32fb2t86ull7ollt20gmcglm8sv893c.apps.googleusercontent.com",
+    iosClientId: "54442420294-8e0714khr3bg3jc7vcp7jvgajqd578ao.apps.googleusercontent.com",
+    webClientId: "54442420294-vuueq211oj24kf8t78t1r0abvlkqcev7.apps.googleusercontent.com",
+    scopes: ["profile", "email"],
+  });
 
   const startSocialAuth = async () => {
     try {
@@ -62,7 +75,7 @@ export default function SignIn() {
           <View className="w-full mt-2">
             <Button
               title="Continuar con Google"
-              handlePress={startSocialAuth}
+              handlePress={promptAsync}
               intent="login2"
               rounded="small"
               modifiers="full-width"
